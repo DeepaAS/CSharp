097aac273a8c42de20eb244537be136afcd70ec3
DeepaAS <31288642+DeepaAS@users.noreply.github.com>
2018-03-08 09:20:51 -0500

    Add files via upload

---
+++ programmrChallenges/ArrayReverse/ArrayReverse/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ArrayReverse
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = new int[3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Enter the array elements[" + i + "]:");
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            Array.Reverse(arr);
+            //end 
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+        }
+    }
+}
---
+++ programmrChallenges/ReverseArray/ReverseArray/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ReverseArray
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string output = string.Empty;
+            string[] inpArr = Console.ReadLine().Split(new char[] { ',' });
+            Array.Reverse(inpArr);
+            foreach (var i in inpArr)
+            {
+                if (i.Length != 0)
+                {
+                    Console.Write(i);
+                    Console.Write(" ");
+                }
+            }
+        }
+    }
+}
